f081857c3994767a86a7ebceab7221deec45a732
HoangTran <trannhathoang231@gmail.com>
2022-07-12 09:55:06 +0700

    admin.js and admin.html

--- NghiepVuGioHang/app/service/SanPhamServices.js
+++ NghiepVuGioHang/app/service/SanPhamServices.js
@@ -2,34 +2,34 @@
      getListSp() {
          return axios({
              method: "get",
-             url: "https://62a2fa6121232ff9b214cbba.mockapi.io/Capstone",
+             url: "https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone",
              // responseType: 'stream'
          });
      }
      addSpAPI(food) {
          return axios({
              method: "post",
-             url: "https://62a2fa6121232ff9b214cbba.mockapi.io/Capstone",
+             url: "https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone",
              data: food,
          });
      }
      getSpDetail(id) {
          return axios({
              method: "get",
-             url: `https://62a2fa6121232ff9b214cbba.mockapi.io/Capstone/${id} `,
+             url: `https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone/${id} `,
          });
      }
      updateSpAPI(id, food) {
          return axios({
              method: "put",
-             url: `https://62a2fa6121232ff9b214cbba.mockapi.io/Capstone/${id} `,
+             url: `https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone/${id} `,
              data: food
          });
      }
      deleteSp(id) {
          return axios({
              method: "delete",
-             url: `https://62a2fa6121232ff9b214cbba.mockapi.io/Capstone/${id} `,
+             url: `https://62a2fc9e21232ff9b2151283.mockapi.io/Products_Capstone/${id} `,
              // responseType: 'stream'
          });
      }
